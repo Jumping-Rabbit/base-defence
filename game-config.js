62c3b93e8ff0704548b9be60f4c57e8b36e82f54
Jumping-Rabbit <ryanliu25@gmail.com>
2024-12-27 00:04:00 -0800

    * Initialize the map with resource mines and the base. * Introduce the concept of game map v.s. viewport. Allow map to pan. * Add 2 new types of buildings to collect resources on mines. * Rename "depot" to "base" and make it expensive.

--- game-config.js
+++ game-config.js
@@ -1,5 +1,19 @@
 var config = {
   background: "#c3d657",
+  map: {
+    cols: 100,
+    rows: 100,
+    resource_mines: [
+      {
+        type: "concrete",
+        numbers: 20,
+      },
+      {
+        type: "metal",
+        numbers: 10,
+      },
+    ], 
+  },
   resources: [
     {
       type: "concrete",
@@ -19,13 +33,14 @@ var config = {
   ],
   buildings: [
     {
-      type: "depot",
-      title: "Depot: store resources",
-      hp: 1000,
+      type: "base",
+      title: "Base: store resources",
+      hp: 2000,
       width: 2,
       height: 2,
       building_cost: {
-        concrete: 10,
+        concrete: 400,
+        metal: 200,
       },
     },
     {
@@ -39,8 +54,41 @@ var config = {
         metal: 5,
       },
       profit: {
-        energy: 5,
-      }
+        energy: 1,
+      },
+      storage: 20,
+    },
+    {
+      type: "concrete_factory",
+      title: "Concrete Factory",
+      hp: 200,
+      width: 1,
+      height: 1,
+      placement_restriction: "concrete",
+      building_cost: {
+        concrete: 5,
+        metal: 5,
+      },
+      profit: {
+        concrete: 1,
+      },
+      storage: 20,
+    },
+    {
+      type: "metal_mill",
+      title: "Metal Mill",
+      hp: 200,
+      width: 1,
+      height: 1,
+      placement_restriction: "metal",
+      building_cost: {
+        concrete: 5,
+        metal: 5,
+      },
+      profit: {
+        metal: 1,
+      },
+      storage: 20,
     },
   ],
 };
--- game-engine.js
+++ game-engine.js
@@ -12,6 +12,7 @@ var game = {
 
 var buildingImages = {};
 var buildingIcons = {};
+var buildingDefinitions = {};
 
 var frameCounter = 0;
 
@@ -51,11 +52,29 @@ function gameEngineInit() {
   setInterval(showFps, 1000);
 
   for (const building of config.buildings) {
+    buildingDefinitions[building.type] = building;
     buildingImages[building.type] = new Image();
     buildingImages[building.type].src = building.type + ".png";
     buildingIcons[building.type] = new Image();
     buildingIcons[building.type].src = building.type + "-icon.png";
   }
+}
+
+function hasBuildingAt(x, y) {
+  for (const building of game.buildings) {
+    const buildingDef = buildingDefinitions[building.type];
+    if (x >= building.x && x < building.x + buildingDef.width && y >= building.y && y < building.y + buildingDef.height) return true;
+  }
+
+  return false;
+}
+
+function hasResourceMineAt(x, y) {
+  for (const resource_mine of game.resource_mines) {
+    if (resource_mine.x == x && resource_mine.y == y) return true;
+  }
+
+  return false;
 }
 
 function newGame(e) {
@@ -68,16 +87,31 @@ function newGame(e) {
     game.resources[resource.type] = resource.initial_amount;
   }
 
+  game.buildings = [{type: "base", x: config.map.cols / 2 - 1, y: config.map.rows / 2 - 1}];
+  game.resource_mines = [];
+
+  for (const resource_mine of config.map.resource_mines) {
+    for (var i = 0; i < resource_mine.numbers; i++) {
+      var x, y;
+      do {
+        x = getRandomInt(config.map.cols);
+        y = getRandomInt(config.map.rows);
+      } while (hasResourceMineAt(x, y) || hasBuildingAt(x, y));
+      game.resource_mines.push({type: resource_mine.type, x: x, y: y});
+    }
+  }
+
+  game.viewport = {
+    x: Math.floor(config.map.cols / 2 - 20),
+    y: Math.floor(config.map.rows / 2 - 16),
+  };
+
   console.log(game);
 }
 
 function keyDown(e) {
-  if (e.repeat) {
-    return; // skip all repeated keydown events
-  }
-
-  console.log(`Key "${e.key}" repeating [event: keydown]`);
-  if (e.key == "Escape") {
+  console.log(`Key "${e.key}" pressed [event: keydown]`);
+  if (e.key == "Escape" && !e.repeat) {
     if (game.status == "running") {
       game.status = "paused";
       mainMenu.style.visibility = "visible";
@@ -86,6 +120,18 @@ function keyDown(e) {
       mainMenu.style.visibility = "hidden";
     }
   }
+  if ((e.key == "w" || e.key == "ArrowUp") && game.viewport.y > 0) {
+    game.viewport.y--;
+  }
+  if ((e.key == "s" || e.key == "ArrowDown") && game.viewport.y < config.map.rows - 32) {
+    game.viewport.y++;
+  }
+  if ((e.key == "a" || e.key == "ArrowLeft") && game.viewport.x > 0) {
+    game.viewport.x--;
+  }
+  if ((e.key == "d" || e.key == "ArrowRight") && game.viewport.x < config.map.cols - 40) {
+    game.viewport.x++;
+  }  
 }
 
 function canvasClicked(e) {
@@ -96,6 +142,7 @@ function canvasClicked(e) {
       var i = Math.floor((mouse.y - 32) / 96);
       game.control_state = "building";
       game.building_mode = {type: config.buildings[i].type};
+      canvas.style.cursor = "grabbing";
     }
   }  
 }
@@ -105,6 +152,7 @@ function canvasRightClicked(e) {
   if (game.control_state == "building") {
     game.control_state = "none";
     delete game.building_mode;
+    canvas.style.cursor = "auto";
   }
 }
 
@@ -114,13 +162,15 @@ function canvasMouseMove(e) {
     if (mouse.x < 1280) {
       game.building_mode.x = Math.floor(mouse.x / 32);
       game.building_mode.y = Math.floor(mouse.y / 32);
+      canvas.style.cursor = "none";
     } else {
       delete game.building_mode.x;
       delete game.building_mode.y;
+      canvas.style.cursor = "grabbing";
     }
+  } else {
+    canvas.style.cursor = "auto";
   }
-
-  canvas.style.cursor = (game.control_state == "building" && mouse.x < 1280) ? "none" : "auto";
 }
 
 const resourceWidgetWidth = 320 / Object.keys(config.resources).length;
@@ -129,7 +179,13 @@ function drawResource(resourceType, amount, canvasCtx, x, y) {
   canvasCtx.fillStyle = config.resourceMap[resourceType].color;
   canvasCtx.fillRect(x, y, 32, 32);
   canvasCtx.fillStyle = "#000";
-  canvasCtx.fillText(amount, x + 40, y + 5);
+  canvasCtx.fillText(amount, x + 36, y + 5);
+}
+
+function rectsOverlap(x1, y1, w1, h1, x2, y2, w2, h2) {
+  const xOverlaped = (x1 >= x2 && x1 < x2 + w2) || (x1 + w1 >= x2 && x1 + w1 < x2 + w2);
+  const yOverlaped = (y1 >= y2 && y1 < y2 + h2) || (y1 + h1 >= y2 && y1 + h1 < y2 + h2);
+  return xOverlaped && yOverlaped;
 }
 
 function frame() {
@@ -141,6 +197,22 @@ function frame() {
   var canvasCtx = canvas.getContext("2d");
   canvasCtx.fillStyle = config.background;
   canvasCtx.fillRect(0, 0, 1280, 1024);
+  // Draw map
+  for (const resource_mine of game.resource_mines) {
+    if (resource_mine.x >= game.viewport.x && resource_mine.x < game.viewport.x + 40 &&
+      resource_mine.y >= game.viewport.y && resource_mine.y < game.viewport.y + 32) {
+        var resource = findResourceDefinition(resource_mine.type);
+        canvasCtx.fillStyle = resource.color;
+        canvasCtx.fillRect((resource_mine.x - game.viewport.x) * 32, (resource_mine.y - game.viewport.y) * 32, 32, 32);
+      }
+  }
+  for (const building of game.buildings) {
+    const buildingDef = buildingDefinitions[building.type];
+    if (rectsOverlap(building.x, building.y, buildingDef.width, buildingDef.height, game.viewport.x, game.viewport.y, 40, 32)) {
+      canvasCtx.drawImage(buildingImages[building.type], (building.x - game.viewport.x) * 32, (building.y - game.viewport.y) * 32);
+    }
+  }
+
   if (game.control_state == "building") {
     canvasCtx.strokeStyle = "black";
     for (var x = 0; x < 40; x++) {
@@ -161,7 +233,7 @@ function frame() {
   canvasCtx.fillRect(controlAreaX, 0, 320, 1024);
 
   var i = 0;
-  canvasCtx.font = "20px serif";
+  canvasCtx.font = "16px serif";
   canvasCtx.textBaseline = "top";
   for (const resource of config.resources) {
     drawResource(resource.type, game.resources[resource.type], canvasCtx, controlAreaX + i * resourceWidgetWidth, 0);
@@ -177,14 +249,14 @@ function frame() {
     canvasCtx.fillText("Cost: ", controlAreaX + 72, buildingYOffset + 37);
     var j = 0;
     for (const resourceType in building.building_cost) {
-      drawResource(resourceType, building.building_cost[resourceType], canvasCtx, controlAreaX + 160 + j * 64, buildingYOffset + 32);
+      drawResource(resourceType, building.building_cost[resourceType], canvasCtx, controlAreaX + 132 + j * 70, buildingYOffset + 32);
       j++;
     }
     if (building.profit) {
       canvasCtx.fillText("Profit: ", controlAreaX + 72, buildingYOffset + 69);
       j = 0;
       for (const resourceType in building.profit) {
-        drawResource(resourceType, building.profit[resourceType], canvasCtx, controlAreaX + 160 + j * 64, buildingYOffset + 64);
+        drawResource(resourceType, building.profit[resourceType], canvasCtx, controlAreaX + 132 + j * 70, buildingYOffset + 64);
         j++;
       }
     }
@@ -197,4 +269,14 @@ function frame() {
 function showFps() {
   fps.textContent = "FPS: " + frameCounter;
   frameCounter = 0;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max); 
+}
+
+function findResourceDefinition(resourceType) {
+  for (const resource of config.resources) {
+    if (resource.type == resourceType) return resource;
+  }
 }
